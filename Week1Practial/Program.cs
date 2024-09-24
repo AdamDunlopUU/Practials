bd66f6d236baae31ed0de1f99460d7f35e09af8d
Adam Dunlop <Dunlop-A15@ulster.ac.uk>
2024-09-24 14:52:16 +0100

    Add project files.

---
+++ Week1Practial/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your name : ");
+String name = Console.ReadLine();
+Console.WriteLine("Please enter your age? : ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Hello " + name + ", you will be " + (age + 5) + " In 5 years");
